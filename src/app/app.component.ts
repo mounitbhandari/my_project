c9e1013d4285e98c4dbc1d996b3a37aade3546e5
ritajaghosh <ritajaghosh@gmail.com>
2022-01-20 19:19:03 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  month =[
+      [26,27,28,29,30,31,1],
+      [2,3,4,5,6,7,8],
+     [9,10,11,12,13,4,15],
+     [16,17,18,19,20,21,22],
+     [23,24,25,26,27,28,29],
+      [30,31,1,2,3,4,5],
+    ];
+
+
+
   title = 'My_project';
 }
